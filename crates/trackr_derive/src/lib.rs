c78d9cc5a1c52ba2a27ec838e1e9728f9016244b
= <jonas-zunker@web.de>
2025-09-13 15:03:25 +0200

    Add better error messages, clean up code generation

--- crates/trackr_derive/src/lib.rs
+++ crates/trackr_derive/src/lib.rs
@@ -9,14 +9,14 @@ struct Tracked {
     ident: Ident,
     data: ast::Data<util::Ignored, TrackedField>,
     generics: syn::Generics,
-    vis: syn::Visibility,
+    vis: Visibility,
 }
 
 #[derive(Debug, FromField)]
 #[darling(attributes(track))]
 struct TrackedField {
     ident: Option<Ident>,
-    vis: syn::Visibility,
+    vis: Visibility,
     ty: Type,
     #[darling(default)]
     skip: bool,
@@ -28,7 +28,7 @@ struct TrackedField {
 
 impl TrackedField {
     pub fn ident(&self) -> Ident {
-        self.ident.clone().unwrap()
+        self.ident.clone().expect("expected named field")
     }
 
     fn is_tracked(&self) -> bool {
@@ -39,22 +39,21 @@ impl TrackedField {
         &self,
         vis: &Visibility,
         flag_ty: &Ident,
-        tracker_field: &Ident,
+        flag_field: &Ident,
     ) -> Option<TokenStream> {
         if self.skip {
             return None;
         }
 
         let vis = if self.pub_ {
-            syn::Visibility::Public(syn::token::Pub::default())
+            Visibility::Public(syn::token::Pub::default())
         } else {
             vis.clone()
         };
 
         let ident = self.ident();
         let ty = &self.ty;
-
-        let get = format_ident!("{ident}");
+        let get: Ident = format_ident!("{ident}");
         let get_mut = format_ident!("{ident}_mut");
 
         Some(quote! {
@@ -65,7 +64,7 @@ impl TrackedField {
             #vis fn #get_mut(&mut self) -> trackr::TrackedField<'_, #ty, #flag_ty> {
                 trackr::TrackedField::new(
                     #flag_ty::#ident,
-                    &mut self.#tracker_field,
+                    &mut self.#flag_field,
                     &mut self.#ident
                 )
             }
@@ -75,51 +74,53 @@ impl TrackedField {
 }
 
 /// Gets the type for the bitflags
-fn bits_ty(n: usize) -> syn::Type {
+fn bits_ty(n: usize) -> Option<syn::Type> {
     match n {
-        n if n < 8 + 1 => syn::parse_quote!(u8),
-        n if n < 16 + 1 => syn::parse_quote!(u16),
-        n if n < 32 + 1 => syn::parse_quote!(u32),
-        n if n < 64 + 1 => syn::parse_quote!(u64),
-        n if n < 128 + 1 => syn::parse_quote!(u128),
-        _ => panic!("Too many tracked fields, maximum is 128"),
+        n if n < 8 + 1 => Some(syn::parse_quote!(u8)),
+        n if n < 16 + 1 => Some(syn::parse_quote!(u16)),
+        n if n < 32 + 1 => Some(syn::parse_quote!(u32)),
+        n if n < 64 + 1 => Some(syn::parse_quote!(u64)),
+        n if n < 128 + 1 => Some(syn::parse_quote!(u128)),
+        _ => None,
     }
 }
 
-impl ToTokens for Tracked {
+struct TrackedOutput<'a> {
+    ident: &'a Ident,
+    vis: &'a Visibility,
+    flag_field: &'a TrackedField,
+    tracked_fields: Vec<&'a &'a TrackedField>,
+    bit_ty: Type,
+    generics: &'a syn::Generics,
+}
+
+impl ToTokens for TrackedOutput<'_> {
     fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
-        let ident = self.ident.clone();
+        let ident = &self.ident;
         let (imp, ty, wher) = self.generics.split_for_impl();
-        let vis = self.vis.clone();
+        let vis = self.vis;
 
-        let fields = self.data.as_ref().take_struct().expect("expected struct");
-
-        // Find the field marked with tracker
-        let tracker_field = fields
-            .iter()
-            .find(|field| field.flag)
-            .expect("expected at least one #[track(field)] field");
-
-        let tracker_field_id = tracker_field
+        // The field that holds the flags
+        let flag_field = self
+            .flag_field
             .ident
             .as_ref()
             .expect("Tracker field must be named");
 
-        // Get all tracked fields
-        let tracked_fields = fields.iter().filter(|field| field.is_tracked());
-        // Get the type for the bitflags
-        let bits_ty = bits_ty(tracked_fields.clone().count());
-
         // Auto generate all flags
         let flag_ty = format_ident!("{ident}Flags");
-        let flags = tracked_fields.clone().enumerate().map(|(i, field)| {
-            let ident = field.ident.as_ref().unwrap();
+        let flags = self.tracked_fields.iter().enumerate().map(|(i, field)| {
+            let ident = field.ident.as_ref().expect("expected named field");
             quote!( const #ident = 1 << #i; )
         });
 
-        let field_impls = tracked_fields
-            .clone()
-            .map(|field| field.gen_impl(&field.vis, &flag_ty, tracker_field_id));
+        // Generate impls for each tracked field
+        let field_impls = self
+            .tracked_fields
+            .iter()
+            .map(|field| field.gen_impl(&field.vis, &flag_ty, flag_field));
+
+        let bits_ty = &self.bit_ty;
 
         quote!(
             trackr::__reexport::bitflags! {
@@ -137,11 +138,11 @@ impl ToTokens for Tracked {
             impl trackr::TrackedStruct for #ident #ty #wher {
                 type Flags = #flag_ty;
                 fn flags(&self) -> Self::Flags {
-                    self.#tracker_field_id
+                    self.#flag_field
                 }
 
                 fn flags_mut(&mut self) -> &mut Self::Flags {
-                    &mut self.#tracker_field_id
+                    &mut self.#flag_field
                 }
             }
         )
@@ -149,36 +150,19 @@ impl ToTokens for Tracked {
     }
 }
 
-#[proc_macro_derive(Tracked, attributes(track))]
-pub fn tracked(item: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let derive_input = syn::parse_macro_input!(item as syn::DeriveInput);
-
-    let input = match Tracked::from_derive_input(&derive_input) {
-        Ok(input) => input,
-        Err(err) => return err.write_errors().into(),
-    };
+fn tracked_impl(input: Tracked) -> syn::Result<proc_macro2::TokenStream> {
     // Validate before generating tokens so we can emit nice span errors instead of panicking.
-    let fields = match input.data.as_ref().take_struct() {
-        Some(f) => f,
-        None => {
-            return syn::Error::new_spanned(
-                &derive_input.ident,
-                "#[derive(Tracked)] only supports structs",
-            )
-            .to_compile_error()
-            .into();
-        }
-    };
+    let fields = input.data.as_ref().take_struct().ok_or_else(|| {
+        syn::Error::new_spanned(&input.ident, "#[derive(Tracked)] only supports structs")
+    })?;
 
     // Collect all flag fields (#[track(flag)])
     let flag_fields: Vec<_> = fields.iter().filter(|f| f.flag).collect();
     if flag_fields.is_empty() {
-        return syn::Error::new_spanned(
-            &derive_input.ident,
+        return Err(syn::Error::new_spanned(
+            &input.ident,
             "missing a field marked with #[track(flag)]",
-        )
-        .to_compile_error()
-        .into();
+        ));
     }
     if flag_fields.len() > 1 {
         // Combine errors for each additional flag field to give user precise spans.
@@ -191,30 +175,50 @@ pub fn tracked(item: proc_macro::TokenStream) -> proc_macro::TokenStream {
         );
         for extra in &flag_fields[1..] {
             if let Some(id) = &extra.ident {
-                err.combine(syn::Error::new_spanned(id, "additional #[track(flag)] field here"));
+                err.combine(syn::Error::new_spanned(
+                    id,
+                    "additional #[track(flag)] field here",
+                ));
             }
         }
-        return err.to_compile_error().into();
+        return Err(err);
     }
 
-    // Count tracked (non-skip, non-flag) fields to ensure we stay within supported bit width.
-    let tracked_count = fields.iter().filter(|f| f.is_tracked()).count();
-    if tracked_count > 128 {
-        // Use the single flag field's span (or struct ident) for the error anchor.
-        let span_anchor = flag_fields[0]
-            .ident
-            .as_ref()
-            .map(|i| i.span())
-            .unwrap_or(derive_input.ident.span());
-        return syn::Error::new(
-            span_anchor,
-            format!(
-                "too many tracked fields: {tracked_count} (maximum supported is 128)"
-            ),
+    // Safe to unwrap since we checked len() above
+    let flag_field = flag_fields[0];
+
+    // Obtain tracked fields
+    let tracked_fields: Vec<_> = fields.iter().filter(|f| f.is_tracked()).collect();
+    let tracked_count = tracked_fields.len();
+
+    // Determine the bit type we need to store the flags
+    let bit_ty = bits_ty(tracked_count).ok_or_else(|| {
+        syn::Error::new_spanned(
+            &input.ident,
+            format!("too many tracked fields: {tracked_count} (maximum supported is 128)"),
         )
-        .to_compile_error()
-        .into();
-    }
+    })?;
 
-    input.to_token_stream().into()
+    Ok(TrackedOutput {
+        ident: &input.ident,
+        generics: &input.generics,
+        vis: &input.vis,
+        flag_field,
+        tracked_fields,
+        bit_ty,
+    }
+    .to_token_stream())
+}
+
+#[proc_macro_derive(Tracked, attributes(track))]
+pub fn tracked(item: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let derive_input = syn::parse_macro_input!(item as syn::DeriveInput);
+
+    let input = match Tracked::from_derive_input(&derive_input) {
+        Ok(input) => input,
+        Err(err) => return err.write_errors().into(),
+    };
+    tracked_impl(input)
+        .unwrap_or_else(syn::Error::into_compile_error)
+        .into()
 }
